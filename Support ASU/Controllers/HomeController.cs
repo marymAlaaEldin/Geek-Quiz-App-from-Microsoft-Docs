15709cdb3628569d19d9b9febbea8afffc6eb6b2
marymAlaaEldin <marym.alaaeldeen.s@gmail.com>
2023-02-23 16:39:00 +0200

    add files

---
+++ Support ASU/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using TriviaQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TriviaQuiz.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private TriviaContext db = new TriviaContext();
+        [Authorize]
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var data = db.TriviaQuestions.ToList();
+            return View(data);
+        }
+
+        public ActionResult Contact()
+        {
+            var userId = User.Identity.Name;
+            var lastQuestionId = db.TriviaAnswers
+                .Where(a => a.UserId == userId)
+                .GroupBy(a => a.QuestionId)
+                .Select(g => new { QuestionId = g.Key, Count = g.Count() })
+                .OrderByDescending(q => new { q.Count, QuestionId = q.QuestionId })
+                .Select(q => q.QuestionId)
+                .FirstOrDefault();
+            ViewBag.Message = userId;
+            ViewBag.data = lastQuestionId;
+
+            return View();
+        }
+        //private async Task<TriviaQuestion> NextQuestionAsync(string userId)
+        //{
+        //    var lastQuestionId = await this.db.TriviaAnswers
+        //        .Where(a => a.UserId == userId)
+        //        .GroupBy(a => a.QuestionId)
+        //        .Select(g => new { QuestionId = g.Key, Count = g.Count() })
+        //        .OrderByDescending(q => new { q.Count, QuestionId = q.QuestionId })
+        //        .Select(q => q.QuestionId)
+        //        .FirstOrDefaultAsync();
+
+        //    var questionsCount = await this.db.TriviaQuestions.CountAsync();
+
+        //    var nextQuestionId = (lastQuestionId % questionsCount) + 1;
+        //    return await this.db.TriviaQuestions.FindAsync(CancellationToken.None, nextQuestionId);
+        //}
+    }
+}
---
+++ Support ASU/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+using Owin;
+using TriviaQuiz.Models;
+
+[assembly: OwinStartupAttribute(typeof(TriviaQuiz.Startup))]
+namespace TriviaQuiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+       
+    }
+}
